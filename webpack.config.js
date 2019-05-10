8c84920ada2029de30c2bda875ab6cc3ef23c052
Bruce <bruce@yogainternational.com>
2019-05-09 23:37:49 -0400

    updates webpack settings and adds seeding script

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,19 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/App.jsx`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
         },
       },
     ],
