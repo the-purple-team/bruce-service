57d1a4d6177179863dfbe3c7c33e77d8e355884c
Bruce <bruce@yogainternational.com>
2019-06-06 22:55:10 -0400

    adds config files for Enzyme and Jest

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/App.jsx`,
+  mode: 'development'
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
@@ -15,7 +16,7 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
     ],
