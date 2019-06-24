80e0a7aee0e370d3a0e82b7cb9a73ab7c45f07b1
Bruce <bruce@yogainternational.com>
2019-06-23 21:37:37 -0400

    removes home button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Wrapper, NavButton } from './elements.jsx';
+import { Wrapper } from './elements.jsx';
 import Product from './Product.jsx';
 import AllProductsLinkGenerator from './AllProductsLinkGenerator.jsx';
 const $ = require('jquery');
@@ -59,8 +59,6 @@ class App extends React.Component {
   render() {
     return (
       <Wrapper>
-        <h1>Welcome to Amazon!</h1>
-        <NavButton href="/">Home</NavButton>
         {window.location.pathname !== '/' ? (
           <Product currentProduct={this.state.currentProduct} />
         ) : (
