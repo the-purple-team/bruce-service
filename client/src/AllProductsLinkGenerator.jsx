7732baa6bc88fd8925834b5cf3500f86603a940c
Bruce <bruce@yogainternational.com>
2019-06-23 22:54:13 -0400

    adds loading icon to mock homepage

--- client/src/AllProductsLinkGenerator.jsx
+++ client/src/AllProductsLinkGenerator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyledLink } from './elements.jsx';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export const Link = ({ id, name }) => (
   <StyledLink href={`/products/${id}`}>
@@ -8,6 +9,10 @@ export const Link = ({ id, name }) => (
 );
 
 const AllProductsLinkGenerator = ({ allProducts }) =>
-  allProducts.map(({ id, title }) => <Link key={id} id={id} name={title} />);
+  allProducts.length > 1 ? (
+    allProducts.map(({ id, title }) => <Link key={id} id={id} name={title} />)
+  ) : (
+    <CircularProgress style={{ margin: '10% 50%' }} color="rgb(35, 47, 62)" />
+  );
 
 export default AllProductsLinkGenerator;
